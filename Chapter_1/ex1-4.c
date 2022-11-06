7dc0e8d8eabb27c4db340819dba73a052e6205cb
RogueHabanero <RogueHabanero@protonmail.com>
2022-11-06 14:05:43 -0600

    adding ex1-4.c

---
+++ Chapter_1/ex1-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("Celsius\t\t Fahrenheit\n");
+
+    while (celsius <= upper){
+        fahr =  (celsius  * (9.0/5.0)) + 32.0;
+	printf("%3.0f\t\t %6.1f\n", celsius, fahr);
+	celsius += step;
+    }
+}
+
